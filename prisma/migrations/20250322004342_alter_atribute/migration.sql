e177620c0e4af973ea9493ea0dc00eacda93edd2
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-03-22 16:31:13 -0300

    vaaa

--- prisma/migrations/20250322004342_alter_atribute/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[nome]` on the table `usuarios` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "usuarios_nome_key" ON "usuarios"("nome");
